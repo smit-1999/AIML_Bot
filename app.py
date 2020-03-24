d0f09d280f1f71850e5a80eea37101882fd42dc6
smitshah1999@gmail.com <smitshah1999@gmail.com>
2020-03-24 11:21:24 +0530

    adds code for connecting sql,insert query,and fetch query from db

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-
+import mysql.connector
 app = Flask(__name__)
 
 @app.route("/")
@@ -19,5 +19,28 @@ def userQuery(user):
     return jsonify({'you sent this': json['a']})
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1',port=8000)
+    #app.run(host='127.0.0.1',port=8000)
+    cnx = mysql.connector.connect(user='smit', password='smit@123',
+                              host='127.0.0.1',
+                              database='user_chats')
+    mycursor = cnx.cursor()
+    #mycursor.execute("create table customers(name VARCHAR(255))")
+
+    '''
+    code for inserting into db
+    '''
+    # sql = "INSERT INTO chats (time,userid,query,response) VALUES (%s,%s,%s,%s)"
+    # val = ("24/3","smit","insert from code","hope it works!");
+    # mycursor.execute(sql,val)
+    # cnx.commit()
+    # print(mycursor.rowcount,"record inserted")
+
+   
+    a="alice"
+    val=(a,)
+    mycursor.execute("SELECT * FROM chats WHERE userid=%s",val)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+         print(x)
+    cnx.close();                          
     
